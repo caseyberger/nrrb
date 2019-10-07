058ea03f68e03a43a30b0a6909b8d80996fefba3
Donald E. Willcox <eugene.willcox@gmail.com>
2019-10-07 15:27:15 -0700

    WIP conversion of Ka_Re.cpp

--- amrex-langevin/Source/Ka_Re.cpp
+++ amrex-langevin/Source/Ka_Re.cpp
@@ -10,24 +10,34 @@
 #include <sstream>
 #include <vector>
 #include <cstdlib>
-#include "Langevin_evolution.h"
+#include "Langevin.H"
 #include <omp.h>
 /*
 Defines the function that evolves the lattice in Langevin time, as well as the real and imaginary
 drift functions, K_a.
 
-Includes definition of an antisymmetric tensor, epsilon, and functions that modify the location 
+Includes definition of an antisymmetric tensor, epsilon, and functions that modify the location
 on the lattice to one positive or negative step in the direction specified.
 */
-//function declarations:
-double epsilon(int a, int b);
-std::vector<int> positive_step(int dim, int dir, int i, int t, int Nx, int Nt);
-std::vector<int> negative_step(int dim, int dir, int i, int t, int Nx, int Nt);
-std::vector<double> phi_a(double *** Lattice, int i, int t, int a, bool new_lat);
+
+/* enum ComplexComponents { Real = 0, Imag }; */
 
 //COMPUTING THE REAL DRIFT FUNCTION
-double K_a_Re(double m, double l,double w, double w_t, int a, double dtau, double *** Lattice, int dim, int Nx, int Nt, int i, int t, double mu){
-	double Ka = 0.0;
+Real K_a_Re(Real m, Real l,Real w, Real w_t, int a, Real dtau, Real mu, amrex::Array4<amrex::Real> const& Lattice_old, const amrex::GeometryData& geom, int i, int j, int t){
+    const auto domain_xlo = geom.ProbLo();
+    const auto domain_xhi = geom.ProbHi();
+    const auto domain_box = geom.Domain();
+    const auto domain_lo = amrex::lbound(domain_box);
+    const auto domain_hi = amrex::ubound(domain_box);
+    const Real x_center = 0.5 * (domain_xlo[0] + domain_xhi[0]);
+    const Real y_center = 0.5 * (domain_xlo[1] + domain_xhi[1]);
+    const Real dx_cell = geom.CellSize(0);
+    const Real dy_cell = geom.CellSize(1);
+    const Real x_cell = domain_xlo[0] + dx_cell * (i + 0.5 - domain_lo[0]);
+    const Real y_cell = domain_xlo[1] + dy_cell * (j + 0.5 - domain_lo[1]);
+    const Real r2_cell = (x_cell - x_center)**2 + (y_cell - y_center)**2;
+
+	Real Ka = 0.0;
 	//doing -K_{a}^{R} and then returning -Ka at the end for simplicity
 	//modify mu, m, w, wtr, and l by dtau
 	mu = dtau*mu;
@@ -35,121 +45,155 @@ double K_a_Re(double m, double l,double w, double w_t, int a, double dtau, doubl
 	w_t = dtau*w_t;
 	w = dtau*w;
 	l = dtau*l;
-	//define bool new_lat = true if we are using the new lattice 
-	//and bool old_lat = false if we are using the old one
-	bool old_lat = false; 
-	std::vector<int> vp = positive_step(dim, 4, i, t, Nx, Nt);
-	std::vector<int> vn = negative_step(dim, 4, i, t, Nx, Nt);
-	int t_p = vp[1];
-	int t_n = vn[1];
-	std::vector<double> phia = phi_a(Lattice,i,t,a,old_lat);//phi_a
-	std::vector<double> phia_pt = phi_a(Lattice,i,t_p,a,old_lat);//phi_{a,t+1}
-	std::vector<double> phia_mt = phi_a(Lattice,i,t_n,a,old_lat); //phi_{a,t-1}
-	
+
 	//CHEMICAL POTENTIAL + TIME DERIVATIVE PART OF Ka
 	//\phi_{a,r}^{R} - e^{\mu}/2 (\phi_{a,r-t}^{R} + \phi_{a,r+t}^{R})
 	//no special conditions here because we have periodic boundary conditions in time
-	Ka += phia[0]- 0.5*exp(mu)*phia_mt[0] - 0.5*exp(mu)*phia_pt[0];
+	Ka += Lattice_old(i,j,t,field_comp(a,true));
+	Ka += - 0.5 * exp(mu) * Lattice_old(i,j,t-1,field_comp(a,true));
+	Ka += - 0.5 * exp(mu) * Lattice_old(i,j,t+1,field_comp(a,true));
+
 	for (int b=1; b<=2; b++){
-		std::vector<double> phib_mt = phi_a(Lattice,i,t_n,b,old_lat); //phi_{b,t-1}
-		std::vector<double> phib_pt = phi_a(Lattice,i,t_p,b,old_lat); //phi_{b,t+1}
 		//\eps_{ab} e^{\mu}/2 (\phi_{b,r-t}^{I}-\phi_{b,r+t}^{I})
-		Ka += epsilon(a,b)*0.5*exp(mu)*phib_mt[1];
-		Ka -= epsilon(a,b)*0.5*exp(mu)*phib_pt[1];
-	}//last checked on 01.31.19
-	
+		Ka += epsilon(a,b) * 0.5 * exp(mu) * Lattice_old(i,j,t-1,field_comp(b,false));
+		Ka -= epsilon(a,b) * 0.5 * exp(mu) * Lattice_old(i,j,t+1,field_comp(b,false));
+	}//last checked on 10.07.19
+
 	//SPATIAL DERIVATIVE PART OF Ka
 	//To test 0+1 dimensional case, just skip this loop
-	//(d/m)\phi_{a}^{R} 
-	Ka += 1.*dim*phia[0]/m;
-	for (int d = 1; d <= dim; d++){//loop over adjacent spatial sites!
-		std::vector<int> vp = positive_step(dim, d, i, t, Nx, Nt);
-		std::vector<int> vn = negative_step(dim, d, i, t, Nx, Nt);
-		int i_p = vp[0];
-		int i_n = vn[0];
-		std::vector<double> phia_pi = phi_a(Lattice,i_p,t,a,old_lat);//phi_{a,i+1} --> i = x, y, z
-		std::vector<double> phia_mi = phi_a(Lattice,i_n,t,a,old_lat);//phi_{a,i-1} --> i = x, y, z
+	//(d/m)\phi_{a}^{R}
+	Ka += 1.*dim*Lattice(i,j,t,field_comp(a,true))/m;
+	for (int d = 1; d <= AMREX_SPACEDIM-1; d++) { //loop over adjacent spatial sites!
+		//if r is on the border, this term is zero because it's a derivative of \phi_{a,r}\phi_{a,r+i}^{R}
 		// -(1/2m)\phi_{a,i+1}^{R} - (1/2m)\phi_{a,ii1}^{R}
-		//if r is on the border, this term is zero because it's a derivative of \phi_{a,r}\phi_{a,r+i}^{R}
-		if (!(i_p == 9999. || i_n == 9999.)){
-				Ka += -0.5*phia_pi[0]/m -0.5*phia_mi[0]/m;
-			}
-	}//updated on 10.14.18 - part mixing a and b removed per notes
-	//updated on 1.31.19
-	
+        if (d == 1)
+        {
+            if (!(i == domain_lo[0] || i == domain_hi[0]))
+            {
+				Ka += -0.5 * Lattice_old(i+1,j,t,field_comp(a,true)) / m;
+				Ka += -0.5 * Lattice_old(i-1,j,t,field_comp(a,true)) / m;
+            }
+        } else if (d == 2)
+        {
+            if (!(j == domain_lo[1] || j == domain_hi[1]))
+            {
+				Ka += -0.5 * Lattice_old(i,j+1,t,field_comp(a,true)) / m;
+				Ka += -0.5 * Lattice_old(i,j-1,t,field_comp(a,true)) / m;
+            }
+        }
+	}//updated on 10.07.19 - part mixing a and b removed per notes
+
 	//ROTATIONAL AND TRAPPING PARTS OF Ka - ONLY WORKS IN 2D AND IF OMEGA > 0.
-	if (dim ==2){
-		int xint = i%(Nx);
-		int yint = ((i - xint)/Nx)%Nx;
-		//shift x, y by (Nx-1)/2
-		double x = 1.0*xint - 0.5*(Nx-1);
-		double y = 1.0*yint - 0.5*(Nx-1);
-		double r2 = x*x+y*y;
-		std::vector<int> vpx = positive_step(dim, 1, i, t, Nx, Nt);
-		std::vector<int> vnx = negative_step(dim, 1, i, t, Nx, Nt);
-		std::vector<int> vpy = positive_step(dim, 2, i, t, Nx, Nt);
-		std::vector<int> vny = negative_step(dim, 2, i, t, Nx, Nt);
-		std::vector<double> phia_px = phi_a(Lattice,vpx[0],t_p,a,old_lat);//phi_{a,r+x+t}
-		std::vector<double> phia_mx = phi_a(Lattice,vnx[0],t_n,a,old_lat);//phi_{a,r-x-t}
-		std::vector<double> phia_py = phi_a(Lattice,vpy[0],t_p,a,old_lat);//phi_{a,r+y+t}
-		std::vector<double> phia_my = phi_a(Lattice,vny[0],t_n,a,old_lat);//phi_{a,r-y-t}
-		//TRAPPING PIECE
-		// 0.25*m*w_t^{2}*r2*(\phi_{a,r+t}^{R}+\phi_{a,r+t}^{R})
-		Ka += 0.25*m*w_t*w_t*r2*phia_pt[0]+0.25*m*w_t*w_t*r2*phia_mt[0];
-		//ROTATIONAL PIECE
-		// w (x-y)(\phi_{a,r+t}^{I}+\phi_{a,r-t}^{I})
-		Ka += 0.5*w*x*phia_pt[1] - 0.5*w*y*phia_pt[1] + 0.5*w*x*phia_mt[1] - 0.5*w*y*phia_mt[1];
-		// (w/2) y (\phi_{a,r+x}^{I} + \phi_{a,r-x}^{I}) - (w/2) x (\phi_{a,r+y}^{I} + \phi_{a,r-y}^{I}) 
-		//if r is on the border, this term is zero because it's a derivative of \phi_{a,r}\phi_{a,r+i}^{R}
-		if (!(vpx[0] == 9999. || vnx[0] == 9999.)){
-			Ka += 0.5*w*y*phia_px[1] + 0.5*w*y*phia_mx[1];
-		}
-		if (!(vpy[0] == 9999. || vny[0] == 9999.)){
-			Ka += -0.5*w*x*phia_py[1] - 0.5*w*x*phia_my[1];
-		}
-		for (int b=1; b<=2; b++){
-			std::vector<double> phib_mx = phi_a(Lattice,vnx[0],t_n,b,old_lat);//phi_{b,r-x-t}
-			std::vector<double> phib_my= phi_a(Lattice,vny[0],t_n,b,old_lat);//phi_{b,r-y-t}
-			std::vector<double> phib_px = phi_a(Lattice,vpx[0],t_p,b,old_lat);//phi_{b,r+x+t}
-			std::vector<double> phib_py= phi_a(Lattice,vpy[0],t_p,b,old_lat);//phi_{b,r+y+t}
-			std::vector<double> phib_mt = phi_a(Lattice,i,t_n,b,old_lat); //phi_{b,t-1}
-			std::vector<double> phib_pt = phi_a(Lattice,i,t_p,b,old_lat); //phi_{b,t+1}
-			//\eps_{ab} (w/2) (y (\phi_{b,r-x}^{R} + \phi_{b,r+x}^{R})- x (\phi_{b,r-y}^{R}+\phi_{b,r+y}^{R}))
-			Ka += -0.5*epsilon(a,b)*w*x*phib_py[0] - 0.5*epsilon(a,b)*w*x*phib_my[0];
-			Ka += 0.5*epsilon(a,b)*w*y*phib_mx[0] + 0.5*epsilon(a,b)*w*y*phib_px[0];
-			//\eps_{ab} (w/2) (x-y)(\phi_{b,r+t}^{R}+\phi_{b,r-t}^{R})
-			Ka += 0.5*epsilon(a,b)*w*x*phib_pt[0] +0.5*epsilon(a,b)*w*x*phib_mt[0];
-			Ka += -0.5*epsilon(a,b)*w*y*phib_pt[0] -0.5*epsilon(a,b)*w*y*phib_mt[0];
-			//TRAPPING PIECE
-			// -0.25*m*w_t^{2} r2*\eps_{ab}(\phi_{b,r+t}^{I}+\phi_{b,r-t}^{I})
-			Ka += -0.25*m*w_t*w_t*r2*epsilon(a,b)*phib_pt[1] -0.25*m*w_t*w_t*r2*epsilon(a,b)*phib_mt[1];
-		}
-	}
-	//last updated 2.20.19
+	//TRAPPING PIECE
+
+	// 0.25*m*w_t^{2}*r2*(\phi_{a,r+t}^{R}+\phi_{a,r-t}^{R})
+    Ka += 0.25*m*w_t*w_t*r2_cell * Lattice_old(i,j,t+1,field_comp(a,true));
+    Ka += 0.25*m*w_t*w_t*r2_cell * Lattice_old(i,j,t-1,field_comp(a,true));
+
+    for (int b=1; b<=2; b++){
+        //TRAPPING PIECE
+        // -0.25*m*w_t^{2} r2*\eps_{ab}(\phi_{b,r+t}^{I}+\phi_{b,r-t}^{I})
+        Ka += -0.25*m*w_t*w_t*r2_cell*epsilon(a,b) * Lattice_old(i,j,t+1,field_comp(b,false));
+        Ka += -0.25*m*w_t*w_t*r2_cell*epsilon(a,b) * Lattice_old(i,j,t-1,field_comp(b,false));
+    }
+
+    // ROTATIONAL PIECE
+#if (AMREX_SPACEDIM == 3)
+    const Real x = x_cell - x_center;
+    const Real y = y_cell - y_center;
+
+	// w (x-y)(\phi_{a,r+t}^{I}+\phi_{a,r-t}^{I})
+	Ka += 0.5*w*x * Lattice_old(i,j,t+1,field_comp(a, false));
+	Ka += -0.5*w*y * Lattice_old(i,j,t+1,field_comp(a, false));
+	Ka += 0.5*w*x * Lattice_old(i,j,t-1,field_comp(a, false));
+	Ka += -0.5*w*y * Lattice_old(i,j,t-1,field_comp(a, false));
+
+	// (w/2) y (\phi_{a,r+x+t}^{I} + \phi_{a,r-x-t}^{I}) - (w/2) x (\phi_{a,r+y+t}^{I} + \phi_{a,r-y-t}^{I})
+    //if r is on the border, this term is zero because it's a derivative of \phi_{a,r}\phi_{a,r+i}^{R}
+    if (!(i == domain_lo[0] || i == domain_hi[0]))
+    {
+	    Ka += 0.5*w*y * Lattice_old(i+1,j,t+1,field_comp(a,false));
+	    Ka += 0.5*w*y * Lattice_old(i-1,j,t-1,field_comp(a,false));
+    }
+
+    if (!(j == domain_lo[1] || j == domain_hi[1]))
+    {
+	    Ka += -0.5*w*x * Lattice_old(i,j+1,t+1,field_comp(a,false));
+	    Ka += -0.5*w*x * Lattice_old(i,j-1,t-1,field_comp(a,false));
+    }
+
+    for (int b=1; b<=2; b++){
+        //\eps_{ab} (w/2) (y (\phi_{b,r-x-t}^{R} + \phi_{b,r+x+t}^{R})- x (\phi_{b,r-y-t}^{R}+\phi_{b,r+y+t}^{R}))
+        Ka += -0.5*epsilon(a,b)*w*x * Lattice_old(i,j+1,t+1,field_comp(b,true));
+        Ka += - 0.5*epsilon(a,b)*w*x * Lattice_old(i,j-1,t-1,field_comp(b,true));
+
+        Ka += 0.5*epsilon(a,b)*w*y * Lattice_old(i-1,j,t-1,field_comp(b,true));
+        Ka += 0.5*epsilon(a,b)*w*y * Lattice_old(i+1,j,t+1,field_comp(b,true));
+
+        //\eps_{ab} (w/2) (x-y)(\phi_{b,r+t}^{R}+\phi_{b,r-t}^{R})
+        Ka += 0.5*epsilon(a,b)*w*x * Lattice_old(i,j,t+1,field_comp(b,true));
+        Ka += 0.5*epsilon(a,b)*w*x * Lattice_old(i,j,t-1,field_comp(b,true));
+
+        Ka += -0.5*epsilon(a,b)*w*y * Lattice_old(i,j,t+1,field_comp(b,true));
+        Ka += -0.5*epsilon(a,b)*w*y * Lattice_old(i,j,t-1,field_comp(b,true));
+    }
+}
+//last updated 10.07.19
+#endif
 
 	//INTERACTION PART OF Ka
 	for (int b=1; b<=2; b++){
-		std::vector<double> phib = phi_a(Lattice,i,t,b,old_lat);//\phi_{b}
-		std::vector<double> phib_mt = phi_a(Lattice,i,t_n,b,old_lat);//phi_{b,t-1}
-		std::vector<double> phib_pt = phi_a(Lattice,i,t_p,b,old_lat);//phi_{b,t+1}
-		Ka += l*phib[0]*phia_mt[0]*phib_mt[0] - l*phib[0]*phia_mt[1]*phib_mt[1];
-		Ka += l*phib[0]*phia_pt[0]*phib_pt[0] - l*phib[0]*phia_pt[1]*phib_pt[1];
-		Ka += -1.0*l*phib[1]*phia_mt[0]*phib_mt[1] - l*phib[1]*phia_mt[1]*phib_mt[0];
-		Ka += -1.0*l*phib[1]*phia_pt[0]*phib_pt[1] - l*phib[1]*phia_pt[1]*phib_pt[0];
-		Ka += 0.5*l*phia[0]*phib_mt[1]*phib_mt[1]-0.5*l*phia[0]*phib_mt[0]*phib_mt[0];
-		Ka += 0.5*l*phia[0]*phib_pt[1]*phib_pt[1]-0.5*l*phia[0]*phib_pt[0]*phib_pt[0];
-		Ka += l*phia[1]*phib_mt[1]*phib_mt[0]+l*phia[1]*phib_pt[1]*phib_pt[0];
-		Ka += -1.*epsilon(a,b)*l*phia[0]*phia_mt[0]*phib_mt[1]-epsilon(a,b)*l*phia[0]*phia_mt[1]*phib_mt[0];
-		Ka += epsilon(a,b)*l*phia[0]*phia_pt[0]*phib_pt[1]+epsilon(a,b)*l*phia[0]*phia_pt[1]*phib_pt[0];
-		Ka += epsilon(a,b)*l*phia[1]*phia_mt[1]*phib_mt[1]-epsilon(a,b)*l*phia[1]*phia_mt[0]*phib_mt[0];
-		Ka += epsilon(a,b)*l*phia[1]*phia_pt[0]*phib_pt[0]-epsilon(a,b)*l*phia[1]*phia_pt[1]*phib_pt[1];
-		Ka += 0.5*epsilon(a,b)*l*phib[1]*phia_mt[0]*phia_mt[0]-0.5*epsilon(a,b)*l*phib[1]*phia_mt[1]*phia_mt[1];
-		Ka += 0.5*epsilon(a,b)*l*phib[1]*phia_pt[1]*phia_pt[1]-0.5*epsilon(a,b)*l*phib[1]*phia_pt[0]*phia_pt[0];
-		Ka += epsilon(a,b)*l*phib[0]*phia_mt[1]*phia_mt[0]-epsilon(a,b)*l*phib[0]*phia_pt[1]*phia_pt[0];
-		Ka += 0.5*epsilon(a,b)*l*phia[1]*phia_mt[1]*phia_mt[1]-0.5*epsilon(a,b)*l*phia[1]*phia_mt[0]*phia_mt[0];
-		Ka += 0.5*epsilon(a,b)*l*phia[1]*phia_pt[0]*phia_pt[0]-0.5*epsilon(a,b)*l*phia[1]*phia_pt[1]*phia_pt[1];
-		Ka += epsilon(a,b)*l*phia[0]*phia_pt[1]*phia_pt[0]-epsilon(a,b)*l*phia[0]*phia_mt[1]*phia_mt[0];
-	}//last update 2.22.19 to make a gauge interaction
+		Ka += l*Lattice(i,j,t,field_comp(b,true))*Lattice(i,j,t-1,field_comp(a,true))*Lattice(i,j,t-1,field_comp(b,true));
+		Ka += - l*Lattice(i,j,t,field_comp(b,true))*Lattice(i,j,t-1,field_comp(a,false))*Lattice(i,j,t-1,field_comp(b,false));
+
+		Ka += l*Lattice(i,j,t,field_comp(b,true))*Lattice(i,j,t+1,field_comp(a,true))*Lattice(i,j,t+1,field_comp(b,true));
+		Ka += - l*Lattice(i,j,t,field_comp(b,true))*Lattice(i,j,t+1,field_comp(a,false))*Lattice(i,j,t+1,field_comp(b,false));
+
+		Ka += -1.0*l*Lattice(i,j,t,field_comp(b,false))*Lattice(i,j,t-1,field_comp(a,true))*Lattice(i,j,t-1,field_comp(b,false));
+		Ka += - l*Lattice(i,j,t,field_comp(b,false))*Lattice(i,j,t-1,field_comp(a,false))*Lattice(i,j,t-1,field_comp(b,true));
+
+		Ka += -1.0*l*Lattice(i,j,t,field_comp(b,false))*Lattice(i,j,t+1,field_comp(a,true))*Lattice(i,j,t+1,field_comp(b,false));
+		Ka += - l*Lattice(i,j,t,field_comp(b,false))*Lattice(i,j,t+1,field_comp(a,false))*Lattice(i,j,t+1,field_comp(b,true));
+
+		Ka += 0.5*l*Lattice(i,j,t,field_comp(a,true))*Lattice(i,j,t-1,field_comp(b,false))*Lattice(i,j,t-1,field_comp(b,false));
+		Ka += -0.5*l*Lattice(i,j,t,field_comp(a,true))*Lattice(i,j,t-1,field_comp(b,true))*Lattice(i,j,t-1,field_comp(b,true));
+
+		Ka += 0.5*l*Lattice(i,j,t,field_comp(a,true))*Lattice(i,j,t+1,field_comp(b,false))*Lattice(i,j,t+1,field_comp(b,false));
+		Ka += -0.5*l*Lattice(i,j,t,field_comp(a,true))*Lattice(i,j,t+1,field_comp(b,true))*Lattice(i,j,t+1,field_comp(b,true));
+
+		Ka += l*Lattice(i,j,t,field_comp(a,false))*Lattice(i,j,t-1,field_comp(b,false))*Lattice(i,j,t-1,field_comp(b,true));
+		Ka += l*Lattice(i,j,t,field_comp(a,false))*Lattice(i,j,t+1,field_comp(b,false))*Lattice(i,j,t+1,field_comp(b,true));
+
+		Ka += -1.*epsilon(a,b)*l*Lattice(i,j,t,field_comp(a,true))*Lattice(i,j,t-1,field_comp(a,true))*Lattice(i,j,t-1,field_comp(b,false));
+		Ka += -epsilon(a,b)*l*Lattice(i,j,t,field_comp(a,true))*Lattice(i,j,t-1,field_comp(a,false))*Lattice(i,j,t-1,field_comp(b,true));
+
+		Ka += epsilon(a,b)*l*Lattice(i,j,t,field_comp(a,true))*Lattice(i,j,t+1,field_comp(a,true))*Lattice(i,j,t+1,field_comp(b,false));
+		Ka += epsilon(a,b)*l*Lattice(i,j,t,field_comp(a,true))*Lattice(i,j,t+1,field_comp(a,false))*Lattice(i,j,t+1,field_comp(b,true));
+
+		Ka += epsilon(a,b)*l*Lattice(i,j,t,field_comp(a,false))*Lattice(i,j,t-1,field_comp(a,false))*Lattice(i,j,t-1,field_comp(b,false));
+		Ka += -epsilon(a,b)*l*Lattice(i,j,t,field_comp(a,false))*Lattice(i,j,t-1,field_comp(a,true))*Lattice(i,j,t-1,field_comp(b,true));
+
+		Ka += epsilon(a,b)*l*Lattice(i,j,t,field_comp(a,false))*Lattice(i,j,t+1,field_comp(a,true))*Lattice(i,j,t+1,field_comp(b,true));
+		Ka += -epsilon(a,b)*l*Lattice(i,j,t,field_comp(a,false))*Lattice(i,j,t+1,field_comp(a,false))*Lattice(i,j,t+1,field_comp(b,false));
+
+		Ka += 0.5*epsilon(a,b)*l*Lattice(i,j,t,field_comp(b,false))*Lattice(i,j,t-1,field_comp(a,true))*Lattice(i,j,t-1,field_comp(a,true));
+		Ka += -0.5*epsilon(a,b)*l*Lattice(i,j,t,field_comp(b,false))*Lattice(i,j,t-1,field_comp(a,false))*Lattice(i,j,t-1,field_comp(a,false));
+
+		Ka += 0.5*epsilon(a,b)*l*Lattice(i,j,t,field_comp(b,false))*Lattice(i,j,t+1,field_comp(a,false))*Lattice(i,j,t+1,field_comp(a,false));
+		Ka += -0.5*epsilon(a,b)*l*Lattice(i,j,t,field_comp(b,false))*Lattice(i,j,t+1,field_comp(a,true))*Lattice(i,j,t+1,field_comp(a,true));
+
+		Ka += epsilon(a,b)*l*Lattice(i,j,t,field_comp(b,true))*Lattice(i,j,t-1,field_comp(a,false))*Lattice(i,j,t-1,field_comp(a,true));
+		Ka += -epsilon(a,b)*l*Lattice(i,j,t,field_comp(b,true))*Lattice(i,j,t+1,field_comp(a,false))*Lattice(i,j,t+1,field_comp(a,true));
+
+		Ka += 0.5*epsilon(a,b)*l*Lattice(i,j,t,field_comp(a,false))*Lattice(i,j,t-1,field_comp(a,false))*Lattice(i,j,t-1,field_comp(a,false));
+		Ka += -0.5*epsilon(a,b)*l*Lattice(i,j,t,field_comp(a,false))*Lattice(i,j,t-1,field_comp(a,true))*Lattice(i,j,t-1,field_comp(a,true));
+
+		Ka += 0.5*epsilon(a,b)*l*Lattice(i,j,t,field_comp(a,false))*Lattice(i,j,t+1,field_comp(a,true))*Lattice(i,j,t+1,field_comp(a,true));
+		Ka += -0.5*epsilon(a,b)*l*Lattice(i,j,t,field_comp(a,false))*Lattice(i,j,t+1,field_comp(a,false))*Lattice(i,j,t+1,field_comp(a,false));
+
+		Ka += epsilon(a,b)*l*Lattice(i,j,t,field_comp(a,true))*Lattice(i,j,t+1,field_comp(a,false))*Lattice(i,j,t+1,field_comp(a,true));
+		Ka += -epsilon(a,b)*l*Lattice(i,j,t,field_comp(a,true))*Lattice(i,j,t-1,field_comp(a,false))*Lattice(i,j,t-1,field_comp(a,true));
+	}//last update 10.07.19 to make a gauge interaction
 	/*
 	std::stringstream mu_stream, w_stream, dtau_stream;
 	mu_stream << std::fixed << std::setprecision(3) << mu; //truncate mu for filename
