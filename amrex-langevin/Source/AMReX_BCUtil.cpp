56ad148fd7fa8739e4048a9fb3823fea50495669
Donald E. Willcox <eugene.willcox@gmail.com>
2020-03-20 16:47:47 -0700

    It compiles with USE_CUDA=TRUE!!
    Added GPU version of external Dirichlet routine.

--- amrex-langevin/Source/AMReX_BCUtil.cpp
+++ amrex-langevin/Source/AMReX_BCUtil.cpp
@@ -12,12 +12,12 @@ void dummy_cpu_fill_extdir (Box const& bx, Array4<Real> const& dest,
                             const BCRec* bcr, const int bcomp,
                             const int orig_comp)
 {
-    // do something for external Dirichlet (BCType::ext_dir) if needed
+    // set external Dirichlet (BCType::ext_dir) BCs to 0.0
     const auto& domain = geom.Domain();
     const auto& dom_lo = amrex::lbound(domain);
     const auto& dom_hi = amrex::ubound(domain);
 
-    ParallelFor(bx, numcomp, [=](int i, int j, int k, int mcomp) {
+    ParallelFor(bx, numcomp, [=] AMREX_GPU_DEVICE (int i, int j, int k, int mcomp) {
         int n = dcomp + mcomp;
 
         if ((i < dom_lo.x && bcr[n].lo(0) == BCType::ext_dir) ||
@@ -43,7 +43,29 @@ struct dummy_gpu_fill_extdir
                      const BCRec* bcr, const int bcomp,
                      const int orig_comp) const
         {
-            // do something for external Dirichlet (BCType::ext_dir) if there are
+            // set external Dirichlet (BCType::ext_dir) BCs to 0.0
+            const auto& domain = geom.Domain();
+            const auto& dom_lo = amrex::lbound(domain);
+            const auto& dom_hi = amrex::ubound(domain);
+
+            const auto cell = iv.dim3();
+            const int i = cell.x;
+            const int j = cell.y;
+            const int k = cell.z;
+
+            for (int n = dcomp; n < dcomp + numcomp; ++n) {
+                if ((i < dom_lo.x && bcr[n].lo(0) == BCType::ext_dir) ||
+                    (i > dom_hi.x && bcr[n].hi(0) == BCType::ext_dir) ||
+
+                    (j < dom_lo.y && bcr[n].lo(1) == BCType::ext_dir) ||
+                    (j > dom_hi.y && bcr[n].hi(1) == BCType::ext_dir) ||
+
+                    (k < dom_lo.z && bcr[n].lo(2) == BCType::ext_dir) ||
+                    (k > dom_hi.z && bcr[n].hi(2) == BCType::ext_dir))
+                {
+                        dest(i, j, k, n) = 0.0;
+                }
+            }
         }
 };
 
