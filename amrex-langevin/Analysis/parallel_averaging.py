6b74c9deaab926d11c712a4cbec71ca015f9deb5
caseyberger <cberger3@live.unc.edu>
2020-06-09 14:26:01 -0400

    Averages in parallel instead of serial

---
+++ amrex-langevin/Analysis/parallel_averaging.py
@@ -0,0 +1,41 @@
+import os
+import numpy as np
+import multiprocessing as mp
+from defs_cori import *
+
+def serial_averaging(subdir):
+	count = 0
+	curr_dir = os.getcwd()
+	if subdir.startswith("nrrb_data_"):
+		count = count + 1
+		work_dir = curr_dir+subdir
+		if os.path.isdir(work_dir):
+			all_files = os.listdir(work_dir)
+			input_filename = "null"
+			obs_filename = "null"
+			for filename in all_files:
+				if filename.startswith("input"):
+					input_filename = filename
+				if filename.startswith("logfile"): 
+					obs_filename = filename
+			if input_filename == "null":
+				print("no input file")
+				pass
+			elif obs_filename == "null":
+				print("no observables file")
+				pass
+			else:
+				p = extract_parameters(work_dir,input_filename)
+				Langevin_evol_data = get_raw_data(work_dir, obs_filename)
+				Ntherm = int(0.2*len(Langevin_evol_data["Re n"]))
+				average_observables(work_dir,Langevin_evol_data,p,Ntherm)
+	return count
+
+#num_processors = mp.cpu_count()
+num_processors = 4
+pool = mp.Pool(num_processors)
+
+curr_dir = os.getcwd()+"/"
+subdirectories = os.listdir(curr_dir)
+count = pool.map(serial_averaging, [subdir for subdir in subdirectories])
+concatenate_obs_files(curr_dir)
