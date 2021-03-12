d89c93986d7580db5e9feb2b3a5e1c630a24b4d2
Donald E. Willcox <eugene.willcox@gmail.com>
2021-03-11 22:53:59 -0800

    add an option (on by default with HDF5) to append HDF5 output into the same file, grouped by step number

--- amrex-langevin/Source/Langevin_io.cpp
+++ amrex-langevin/Source/Langevin_io.cpp
@@ -7,8 +7,6 @@ void WritePlotfile(int Langevin_step, const Real Langevin_time,
                    const MultiFab& lattice_aux, const Vector<std::string>& auxiliary_names,
                    const Geometry& geom, const NRRBParameters& nrrb)
 {
-    const std::string& pltfile = amrex::Concatenate("plt",Langevin_step,5);
-
     // consolidate field variable names and auxiliary variable names into a single vector
     Vector<std::string> plot_vars;
     for (auto s : component_names) plot_vars.push_back(s);
@@ -25,9 +23,11 @@ void WritePlotfile(int Langevin_step, const Real Langevin_time,
     MultiFab::Copy(lattice_plot, lattice_aux, 0, lattice.nComp(), AIdx::NAux, 0);
 
     // write lattice_plot to a plotfile
-#ifdef AMREX_USE_HDF5
-    WriteSingleLevelPlotfileHDF5(pltfile, lattice_plot, plot_vars, geom, Langevin_time, Langevin_step);
-#else
-    WriteSingleLevelPlotfile(pltfile, lattice_plot, plot_vars, geom, Langevin_time, Langevin_step);
-#endif
+    const std::string& pltfile = amrex::Concatenate("plt",Langevin_step,5);
+    if (nrrb.use_hdf5) {
+        const std::string& pltfile_h = nrrb.append_hdf5 ? "lattice" : pltfile;
+        WriteSingleLevelPlotfileHDF5(pltfile_h, lattice_plot, plot_vars, geom, Langevin_time, Langevin_step, nrrb.append_hdf5);
+    } else {
+        WriteSingleLevelPlotfile(pltfile, lattice_plot, plot_vars, geom, Langevin_time, Langevin_step);
+    }
 }
